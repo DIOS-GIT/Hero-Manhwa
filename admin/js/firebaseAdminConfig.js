d5010bca248db19ccf36991c44336bc99fe8cecc
Dios <fastfutbolkwai@gmail.com>
2026-09-02 22:33:18 -0500

    Add Firebase Admin configuration file

---
+++ admin/js/firebaseAdminConfig.js
@@ -0,0 +1,9 @@
+const firebaseAdminConfig = {
+  apiKey: "AIzaSyBiKoFAa8qCUdvZhCn5t3V1-VCiLMdCCzU",
+  authDomain: "card-game-986f3.firebaseapp.com",
+  databaseURL: "https://card-game-986f3-default-rtdb.firebaseio.com",
+  projectId: "card-game-986f3",
+  storageBucket: "card-game-986f3.firebasestorage.app",
+  messagingSenderId: "773716535114",
+  appId: "1:773716535114:web:796ac9d2f8ed52414a83b3"
+};
